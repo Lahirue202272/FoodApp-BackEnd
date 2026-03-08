d4bfaa579102e83ccc3cb1211edf13c939dd49f9
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-03-08 11:27:48 +0530

    Create user entity,dto and repo and General response dto

--- FoodApp/src/main/java/com/phegon/FoodApp/enums/PaymentGateway.java
+++ FoodApp/src/main/java/com/phegon/FoodApp/enums/PaymentGateway.java
@@ -1,9 +1,8 @@
 package com.phegon.FoodApp.enums;
 
 public enum PaymentGateway {
-    STRIPE,
-    PAYPAL,
-    RAZORPAY,
-    PAYSTACK,
-    FLUTTERWAVE
+    PAYPAL,        // International payments
+    PAYHERE,       // Local Sri Lankan payment gateway
+    VERVE,         // Local bank card options (Visa/MasterCard/Amex)
+    EZCASH         // Mobile wallet (Dialog eZ Cash)
 }
